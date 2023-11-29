619a39d9dd2e65693fdcc28983ac1e38b2be6b83
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2023-11-29 14:22:34 +0700

    Using signals to reset the game

--- level.gd
+++ level.gd
@@ -1,5 +1,4 @@
 extends Node2D
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	print("Hello, world!")
+func _on_maze_body_exited(body: Node2D) -> void:
+	get_tree().reload_current_scene()

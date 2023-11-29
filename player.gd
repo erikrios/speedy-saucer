7092fe846030fc575ccde600c8c90c13ee3eceb4
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2023-11-29 13:14:30 +0700

    Getting input

--- player.gd
+++ player.gd
@@ -1,11 +1,14 @@
 extends RigidBody2D
 
+const force := 100
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _physics_process(delta: float) -> void:
+	if Input.is_action_pressed("move_right"):
+		apply_force(Vector2(force, 0))
+	if Input.is_action_pressed("move_left"):
+		apply_force(Vector2(-force, 0))
+	if Input.is_action_pressed("move_up"):
+		apply_force(Vector2(0, -force))
+	if Input.is_action_pressed("move_down"):
+		apply_force(Vector2(0, force))
+	
